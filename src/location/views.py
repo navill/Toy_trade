06117187419ec6d332a8cef40cec11e671bc01a1
jihoon <blue_jihoon@naver.com>
2020-01-24 22:33:15 +0900

    add google map API for test

--- src/location/views.py
+++ src/location/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+from django.views.generic.base import View
+
+
+class GoogleMapView(ListView):
+    template_name = 'location/test_google_map.html'
